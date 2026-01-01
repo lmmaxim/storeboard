f217b4f7a80efc94c311157d50a9baddb8a8f2c8
Lucian Maxim <lmmaxim@gmail.com>
2026-01-01 22:54:27 +0200

    chore: updated the placeholder for the client id as well

--- components/stores/ConnectStoreModal.tsx
+++ components/stores/ConnectStoreModal.tsx
@@ -154,7 +154,7 @@ export function ConnectStoreModal({ open, onOpenChange }: ConnectStoreModalProps
               <Input
                 id="client_id"
                 {...register('client_id')}
-                placeholder="a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6"
+                placeholder="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
                 disabled={isSubmitting}
                 className="font-mono text-sm"
               />
